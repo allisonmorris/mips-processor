33cf1aab5f6bde7f9239c0d972c462e7fa433911
heybob <bfiliczkowski@gmail.com>
2013-05-17 16:27:22 -0700

    Added updated bundle.v

--- bundle.v
+++ bundle.v
@@ -7,28 +7,29 @@ Control bundle:
  2 = ram_read_en
  3 = ram_write_en
  4 = ram_data_size
- 5 = ram_data_signed
- 6 = skip_ram_mux_sel -- data mem mux ^^ memory
- 7 = alu_func 
- 8 =
- 9
-10  
-11
-12        ^^ execute
-13 = reg_write_dest_mux_sel -- instr mux
-14
-15 = alu_in_a_mux_sel -- shift mux
-16 = alu_in_b_mux_sel -- alu mux
-17 = skip_imm_upshift_mux_sel -- lui mux
-18 = imm_signed_mux_sel -- extender mux
-19 = jump_branch_mux_sel
-20 = branch_mux_sel
-21 = jump_mux_sel
-22 = jump_imm_reg_mux_sel  
-23 = reg_write_en
-24 = reg_write_data_mux_sel  ^^ decode/writeback
-25 = pc_inc_en
-26 = instr_nop_mux_sel  ^^ fetch
+ 5 = 
+ 6 = ram_data_signed
+ 7 = skip_ram_mux_sel -- data mem mux ^^ memory
+ 8 = alu_func 
+ 9 =
+10
+11  
+12
+13        ^^ execute
+14 = reg_write_dest_mux_sel -- instr mux
+15
+16 = alu_in_a_mux_sel -- shift mux
+17 = alu_in_b_mux_sel -- alu mux
+18 = skip_imm_upshift_mux_sel -- lui mux
+19 = imm_signed_mux_sel -- extender mux
+20 = jump_branch_mux_sel
+21 = branch_mux_sel
+22 = jump_mux_sel
+23 = jump_imm_reg_mux_sel  
+24 = reg_write_en
+25 = reg_write_data_mux_sel  ^^ decode/writeback
+26 = pc_inc_en
+27 = instr_nop_mux_sel  ^^ fetch
 
 
 Data bundle = 
